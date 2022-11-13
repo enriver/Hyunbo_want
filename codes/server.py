9491b5754207e4fb0af2adcd4ed13af1966ca39a
river <riverkeh@naver.com>
2022-11-13 17:27:23 +0900

    ADD 'Ticker dictionary' set & get functions

--- codes/server.py
+++ codes/server.py
@@ -1,6 +1,7 @@
 from firebase import Firebase
 from krx_info import KRX_Info as KRX
 import pandas as pd
+import json
 
 class Server(Firebase, KRX):
     def __init__(self):
@@ -13,10 +14,33 @@ class Server(Firebase, KRX):
             codeDict = stock.getCodeDict()
             self.db.setDB(codeDict)
 
-    def show_TickerInfo(self):
+            self.db.setAddress('DEFAULT_SETTING/SAVE_CODE_DICT')
+            self.db.setDB(True)
+
+    # {Ticker:종목명} dictionary return
+    def get_TickerInfo(self):
         self.db.setAddress('CODE_DICT')
         codeDict = self.db.getDB()
+        codeDict = codeDict.replace("'","\"")
+        codeDict = json.loads(codeDict)
 
         return codeDict
+
+    # 관심종목 불러오기 {Ticker : 종목명} dictionary return
+    def get_InterestInfo(self):
+        self.db.setAddress('INTEREST_STOCK')
+        interestDict = self.db.getDB()
+
+        if interestDict is None:
+            return None
+
+        return interestDict
+
+    # 관심종목 추가하기 {Ticker : 종목명}
+    def set_InterestInfo(self,tickerJSON):
+        if self.db.getAddress() != 'INTEREST_STOCK':
+            self.db.setAddress('INTEREST_STOCK')
+
+        self.db.updateDB(tickerJSON)
         
 
